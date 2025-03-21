f2cdb3efbd7f733fef2332b8bcf3efb5c6269019
unknown <marccejg@hotmail.com>
2025-03-21 19:19:24 -0300

    puntuaciones2

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ var celular_1 = require("./celular");
 //instanacias
 var primerCelular = new celular_1.Celular("Samnsung", "A20", "Android", 8, 64, true);
 var segundoCelular = new celular_1.Celular("Motorola", "E2", "Android", undefined, undefined, undefined);
-//primerCelular.setMarca("IPhone");
-//primerCelular.setSistemaOp("IOS")
 console.log(primerCelular.mostrarInfo());
 console.log(segundoCelular.mostrarInfo());
 console.log(segundoCelular.prenderApagar());
